40cab2e31fb8748c06657cfb8ba51a7b1ccd7883
Cédric/Win11 <cedric.ronvel@gmail.com>
2022-12-02 05:29:09 -0800

    .GetAppxPackageList() works

--- powershell/getAppxPackageList.ps1
+++ powershell/getAppxPackageList.ps1
@@ -1,19 +1,33 @@
-#Get-AppxPackage | ConvertTo-Json
 
+# Get current appx package list
+$rawList = Get-AppxPackage
 
-#Get-AppxPackage | ForEach-Object { $logo = ( Get-AppxPackageManifest $_).package.properties.logo ; echo @{logo = $logo ; Name = $_.Name } } | ConvertTo-Json 
-
-# Not useful to filter for isBundle -eq $false since the first command already filter that out
-$rawList = Get-AppxPackage *calc* | Where-Object {($_.isFramework -eq $false) -and ($_.isResourcePackage -eq $false)}
-
-# This convert the objects into regular hashtable
+# Filter, extends and convert to regular array of hashtable.
 $list = @();
 $rawList | ForEach-Object {
-	$element = @{};
-	$_ | Get-Member -MemberType *Property | ForEach-Object { $element.($_.name) = $_; } 
-	$list += $element;
+	# First, filter
+	# Not useful to filter for IsBundle -eq $false since the first command already filter that out
+	#if ( ($_.IsFramework -eq $true) -or ($_.IsResourcePackage -eq $true) ) { continue; }
+	if ( ( $_.IsFramework -eq $false ) -and ( $_.IsResourcePackage -eq $false ) -and ( -not ( $_.PackageFullName.Contains( "_neutral_" ) ) ) ) {
+		$manifest = Get-AppxPackageManifest $_;
+		$element = @{};
+		$current = $_;
+
+		# Convert to a regular hashtable
+		$current | Get-Member -MemberType *Property | ForEach-Object { $element.($_.name) = $current.($_.name); } 
+		
+		# Remove unwanted properties
+		$element.remove("Dependencies");
+		$element.remove("PackageUserInformation");
+		$element.remove("ResourceId");
+
+		# Add properties from the manifest
+		$element.IconPath = $element.InstallLocation + '\' + $manifest.package.properties.logo;
+
+		$list += $element;
+	}
 }
 
-$list | ForEach-Object{ $_.bob= "bill" }
+$list | ConvertTo-Json
+#$list.count
 
-$list
