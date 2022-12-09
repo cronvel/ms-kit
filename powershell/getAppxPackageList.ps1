a9c92d9a17ede780ce978dcf07713362179eade4
Cédric Ronvel <cedric.ronvel@gmail.com>
2022-12-09 12:05:26 +0100

    wip

--- powershell/getAppxPackageList.ps1
+++ powershell/getAppxPackageList.ps1
@@ -1,33 +1,16 @@
 
-# Get current appx package list
-$rawList = Get-AppxPackage
-
-# Filter, extends and convert to regular array of hashtable.
-$list = @();
-$rawList | ForEach-Object {
-	# First, filter
-	# Not useful to filter for IsBundle -eq $false since the first command already filter that out
-	#if ( ($_.IsFramework -eq $true) -or ($_.IsResourcePackage -eq $true) ) { continue; }
-	if ( ( $_.IsFramework -eq $false ) -and ( $_.IsResourcePackage -eq $false ) -and ( -not ( $_.PackageFullName.Contains( "_neutral_" ) ) ) ) {
-		$manifest = Get-AppxPackageManifest $_;
-		$element = @{};
-		$current = $_;
-
-		# Convert to a regular hashtable
-		$current | Get-Member -MemberType *Property | ForEach-Object { $element.($_.name) = $current.($_.name); } 
-		
-		# Remove unwanted properties
-		$element.remove("Dependencies");
-		$element.remove("PackageUserInformation");
-		$element.remove("ResourceId");
-
-		# Add properties from the manifest
-		$element.IconPath = Join-Path -Path $element.InstallLocation -ChildPath $manifest.package.properties.logo;
-
-		$list += $element;
+Get-AppxPackage | Where-Object {
+	! $_.IsFramework -and ! $_.IsResourcePackage -and ! $_.PackageFullName.Contains( "_neutral_" )
+} | ForEach-Object {
+	$manifest = Get-AppxPackageManifest $_
+	@{
+		Name = $_.Name
+		PackageFullName = $_.PackageFullName
+		PackageFamilyName = $_.PackageFamilyName
+		InstallLocation = $_.InstallLocation
+		Version = $_.Version
+		IconPath = Join-Path -Path $_.InstallLocation -ChildPath $manifest.package.properties.logo
 	}
-}
+} | ConvertTo-Json
 
-$list | ConvertTo-Json
-#$list.count
 
