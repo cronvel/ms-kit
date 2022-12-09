34f06d00f8429a28c1bb329b84600fbcfe4fc440
Cédric/Win11 <cedric.ronvel@gmail.com>
2022-12-09 10:49:12 -0800

    wip

--- powershell/getAppxPackageList.ps1
+++ powershell/getAppxPackageList.ps1
@@ -1,20 +1,27 @@
 
+$exit = 0
+
 Get-AppxPackage | Where-Object {
 	! $_.IsFramework -and ! $_.IsResourcePackage -and ! $_.PackageFullName.Contains( "_neutral_" )
 } | ForEach-Object {
 	$manifest = Get-AppxPackageManifest $_
-	if ( $manifest.package.applications ) {
+	
+	$application = $manifest.Package.Applications.Application
+	
+	$application = $application | Where-Object { $_.Id -eq "App" }
+	
+	if ( ! $exit -and $application ) {
 		@{
-			test = $manifest.package.applications
-			test2 = $manifest.package.applications.Length
-			#test3 = $manifest.package.applications.getLength()
 			Name = $_.Name
 			PackageFullName = $_.PackageFullName
 			PackageFamilyName = $_.PackageFamilyName
 			InstallLocation = $_.InstallLocation
 			Version = $_.Version
 			IconPath = Join-Path -Path $_.InstallLocation -ChildPath $manifest.package.properties.logo
+			Executable = Join-Path -Path $_.InstallLocation -ChildPath $application.Executable
+			EntryPoint = $application.EntryPoint
 		}
+		#$exit = 1
 	}
 } | ConvertTo-Json
 
