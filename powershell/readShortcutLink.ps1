1642910ade55fab106d5e05e2cc64803fc4319b0
Cédric/Win11 <cedric.ronvel@gmail.com>
2023-01-04 06:13:59 -0800

    Fix .readShortcutLink(), using stdin to overcome cmd.exe command max length

--- powershell/readShortcutLink.ps1
+++ powershell/readShortcutLink.ps1
@@ -1,12 +1,24 @@
 Param(
-    [Parameter(Mandatory=$true,ValueFromRemainingArguments=$true,HelpMessage = "Specify the path to the file.")]
-    [ValidateScript({Test-Path $_})]
-    [string[]]$pathList=@()
+	[Parameter(HelpMessage = "Specify the path to the file.")]
+	[ValidateScript({Test-Path $_})]
+	[string]$Path,
+
+	[Switch]$Stdin,
+
+	[Parameter(ValueFromPipeline)]$input
 )
 
 $shell = New-Object -ComObject WScript.Shell
 
-$pathList | ForEach-Object {
-	$shell.CreateShortcut($_)
-} | ConvertTo-Json
+if ( $Stdin ) {
+	$input | ConvertFrom-Json | Foreach-Object {
+		# This is ONE input, each input being a JSON
+		$_ | Foreach-Object {
+			$shell.CreateShortcut($_)
+		}
+	} | ConvertTo-Json
+}
+else {
+	$shell.CreateShortcut($Path) | ConvertTo-Json
+}
 
